45126d98b11758479e135d30f43fa06ff77fe86a
LenovaPC <LenovaPC@DESKTOP-9FDNQSH>
2020-08-21 15:09:07 +0530

    commit

---
+++ Thread/src/MyThread.java
@@ -0,0 +1,26 @@
+
+public class MyThread extends Thread{
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("chaild thread");
+		}
+	}
+
+	public void start() {
+		System.out.println("morning");
+		
+	}
+}
+class Thread{
+
+	public static void main(String[] args) {
+		MyThread t=new MyThread();
+		t.start();
+		for(int i=0;i<10;i++) {
+			System.out.println("main thread");
+		}
+		
+
+	}
+
+}
